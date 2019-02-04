b19c5d90c11cc87b1293d071c4bc4a987148e6ec
Jabulani Gwenzi <ignertic@icloud.com>
2019-02-04 17:36:02 +0200

    Fix Licence

--- setup.py
+++ setup.py
@@ -9,11 +9,13 @@ setuptools.setup(
     author_email="ilovebugsincode@gmail.com",
     version="0.1.0",
     description="BluedotSMS python wrapper",
+    licence="MIT",
     long_description=long_description,
     url="http://github.com/ignertic/pybluedot",
     packages=setuptools.find_packages(),
     install_requires=['loguru', 'requests'],
     entry_points={"console_scripts" : ["sms=pybluedot.__main__:main" ]},
-    classifiers = ["Programming Language :: Python :: 3",
-        "LICENSE :: OSI Approved :: MIT LICENSE",
-        "Operating System :: OS Independent"])
+    classifiers=[
+		"Programming Language :: Python :: 3",
+		"Operating System :: OS Independent"])
+        
